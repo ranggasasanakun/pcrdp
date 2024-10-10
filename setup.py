e3a87cf49ee9f7b59a5b46ee7da302d3f09727d2
Sr abelha <raposabrty@gmail.com>
2024-10-10 12:57:14 +0000

    Replace setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,6 @@ actions = [
     (266, 287, 4),  # unattended access
     (516, 405, 4),  # install (wait 15sec)
     (50, 100, 1),   # tic launch avica
-    (249, 203, 4),  # next (wait 15sec)
     (365, 220, 1),  # Allow rdp
     (667, 567, 4),  # finish
     (447, 286, 4),  # ss id & upload (launch iperius)

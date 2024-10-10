1d5eb470bc19ae551025763d250b1e3b200274a0
Sr abelha <raposabrty@gmail.com>
2024-10-10 12:04:31 +0000

    Delete Acess.bat

--- Acess.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-reg add "HKLM\System\CurrentControlSet\Control\Terminal Server" /v fDenyTSConnections /t REG_DWORD /d 0 /f
-netsh advfirewall firewall set rule group="Remote Desktop" new enable=yes
-reg add "HKLM\System\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp" /v UserAuthentication /t REG_DWORD /d 1 /f
-start powershell -NoExit -Command "ngrok tcp 3389 --region sa"
-echo Successfully Enabled RDP Acesss ...
-wall.bat

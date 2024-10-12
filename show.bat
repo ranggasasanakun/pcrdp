f8546ffbef62cf04cedb0c73d65d150e32bc613c
Sr abelha <raposabrty@gmail.com>
2024-10-12 12:00:55 +0000

    Update show.bat

--- show.bat
+++ show.bat
@@ -7,12 +7,5 @@ python -c "import pyautogui as pag; pag.click(588, 10, duration=2)"
 
 echo Telegram: https://t.me/TheDisala4U
 
-echo     _        _           
-echo    / \__   _(_) ___ __ _ 
-echo   / _ \ \ / / |/ __/ _` |
-echo  / ___ \ V /| | (_| (_| |
-echo /_/   \_\_/ |_|\___\__,_|
-            
-
 echo User name : runneradmin
 echo User Pass : TheDisa1a

149eb15e05e1b1c1e786695837050e5a2397d3fb
Sr abelha <raposabrty@gmail.com>
2024-10-12 11:39:31 +0000

    Add new file

---
+++ show.bat
@@ -0,0 +1,18 @@
+@echo off
+start "" /MAX "C:\Users\Public\Desktop\VMQuickConfig"
+python -c "import pyautogui as pag; pag.click(143, 487, duration=5)"
+python -c "import pyautogui as pag; pag.click(155, 554, duration=2)"
+python -c "import pyautogui as pag; pag.click(637, 417, duration=2)"
+python -c "import pyautogui as pag; pag.click(588, 10, duration=2)"
+
+echo Telegram: https://t.me/TheDisala4U
+
+    _        _           
+   / \__   _(_) ___ __ _ 
+  / _ \ \ / / |/ __/ _` |
+ / ___ \ V /| | (_| (_| |
+/_/   \_\_/ |_|\___\__,_|
+            
+
+echo User name : runneradmin
+echo User Pass : TheDisa1a

cdf9ebe51cbda6469cb2c71de2fd8913aa8be1ae
Sr abelha <raposabrty@gmail.com>
2024-10-27 10:37:33 +0000

    Update show.bat

--- show.bat
+++ show.bat
@@ -4,6 +4,8 @@ python -c "import pyautogui as pag; pag.click(143, 487, duration=5)"
 python -c "import pyautogui as pag; pag.click(155, 554, duration=2)"
 python -c "import pyautogui as pag; pag.click(637, 417, duration=2)"
 python -c "import pyautogui as pag; pag.click(588, 10, duration=2)"
+sc config Audiosrv start= auto >nul
+sc start audiosrv >nul
 
 echo Telegram: https://t.me/TheDisala4U
 echo Created by a Brazilian

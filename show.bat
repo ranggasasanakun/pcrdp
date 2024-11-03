27a96ec5108c32c3184bd30c553c554a51433e46
Sr abelha <raposabrty@gmail.com>
2024-11-03 12:10:33 +0000

    Upload New File

---
+++ show.bat
@@ -0,0 +1,16 @@
+@echo off
+start "" /MAX "C:\Users\Public\Desktop\VMQuickConfig"
+python -c "import pyautogui as pag; pag.click(143, 487, duration=5)"
+python -c "import pyautogui as pag; pag.click(155, 554, duration=2)"
+python -c "import pyautogui as pag; pag.click(637, 417, duration=2)"
+python -c "import pyautogui as pag; pag.click(588, 10, duration=2)"
+
+echo Telegram: https://t.me/TheDisala4U
+echo Created by a Brazilian: By abelha7w7
+echo Corrected Once Again Thanks to the People Who Reported It Oh and Just a Warning Whoever Teases a Brazilian Is a Retard.
+echo *And That Brazilian Doesn't Take Defiance Home.*
+echo: #AntiXenophobia
+
+
+echo User name : runneradmin
+echo User Pass : TheDisa1a
